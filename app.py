039679e777971ea039200ba8267c8c0d546492f6
breadgineer <giancarlo.errigo@outlook.com>
2022-06-05 23:54:48 +0200

    initial commit

--- app.py
+++ app.py
@@ -1,35 +1,35 @@
 import json, config
 from flask import Flask, request, jsonify, render_template
-# from binance.client import Client
-# from binance.enums import *
-# from unicodedata import name
-# import requests
-# from binance.websockets import BinanceSocketManager
+from binance.client import Client
+from binance.enums import *
+from unicodedata import name
+import requests
+from binance.streams import BinanceSocketManager
+from functools import wraps
+import time
 
 app = Flask(__name__)
 
-# giancarlo = Client(config.user_credentials[0]["API"]["key"], config.user_credentials[0]["API"]["secret"])
-# david = Client(config.user_credentials[1]["API"]["key"], config.user_credentials[1]["API"]["secret"])
-# price = 0
+giancarlo = Client(config.user_credentials[0]["API"]["key"], config.user_credentials[0]["API"]["secret"])
+price = 0
 
-# clients = [
-#     {
-#     "user_name":"Giancarlo",
-#     "client":giancarlo
-#     }
-#     ,
-#     {
-#     "user_name":"David",
-#     "client":david
-#     }
-# ] 
 
-# def num_of_zeros(n):
-#     if n<1:
-#         s = '{:.16f}'.format(n).split('.')[1]
-#         return 1 + len(s) - len(s.lstrip('0'))
-#     else:
-#         return 0
+def delay(sleep_time:int):
+    def decorator(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            time.sleep(sleep_time)
+            print(f"Sleeping {sleep_time} seconds")
+            return function(*args, **kwargs)
+        return wrapper
+    return decorator
+
+def num_of_zeros(n):
+    if n<1:
+        s = '{:.16f}'.format(n).split('.')[1]
+        return 1 + len(s) - len(s.lstrip('0'))
+    else:
+        return 0
 
 # def orderSpot(client, side, quantity, ticker, user_name, order_type=ORDER_TYPE_MARKET):
 #     try:
@@ -40,14 +40,23 @@ app = Flask(__name__)
 #         error="An error was encountered - " + str(e)
 #         # send_message(error, user_name)
 
-# def orderIsolatedMargin(client, side, quantity, ticker, user_name, order_type=ORDER_TYPE_MARKET, iso="TRUE"):
-#     try:
-#         print(f"sending order {order_type} - {side} {quantity} {ticker} " )
-#         order = client.create_margin_order(symbol=ticker, side=side, type=order_type, quantity=quantity, isIsolated=iso)
-#         return order
-#     except Exception as e:
-#         error="An error was encountered - " + str(e)
-#         # send_message(error,user_name)
+def orderFutures(client, side, quantity, ticker, order_type=ORDER_TYPE_MARKET, iso="TRUE"):
+    try:
+        print(f"sending order {order_type} - {side} {quantity} {ticker} " )
+        order = client.futures_create_order(symbol=ticker, side=side, type=order_type, quantity=quantity)
+        return order
+    except Exception as e:
+        error="An error was encountered - " + str(e)
+        # send_message(error,user_name)
+
+def cancelOrderFutures(client, ticker):
+    # try:
+    print(f"cancelling {ticker} order" )
+    order = client.futures_cancel_all_open_orders(symbol=ticker)
+    return order
+    # except Exception as e:
+    #     error="An error was encountered - " + str(e)
+    #     # send_message(error,user_name)
 
 # def buyAmount(client, coin, ticker):
 #     step = float(client.get_symbol_info(symbol=ticker)["filters"][2]["stepSize"]) # pos in list can change when updated
@@ -66,31 +75,29 @@ app = Flask(__name__)
 #     maxSell = round(balanceSell * .995, r)
 #     return maxSell
 
-# def buyAmountIsolatedMargin(client, ticker):
-#     step = float(client.get_symbol_info(symbol=ticker)["filters"][2]["stepSize"]) # pos in list can change when updated
-#     r = num_of_zeros(step)
-#     balanceBuy = float(client.get_isolated_margin_account(symbols=ticker, 
-#     recvWindow=10000)['assets'][0]['quoteAsset']['free'])
-#     close = float(client.get_symbol_ticker(symbol=ticker)['price'])
-#     maxBuy = round(balanceBuy / close * .995, r)
-#     return maxBuy
+def buyAmountFutures(client, ticker):
+    step = float(client.get_symbol_info(symbol=ticker)["filters"][2]["stepSize"]) # pos in list can change when updated
+    r = num_of_zeros(step)
+    balanceBuy = float(client.futures_account_balance(recvWindow=10000)[0]['availableBalance'])
+    close = float(client.get_symbol_ticker(symbol=ticker)['price'])
+    maxBuy = round(balanceBuy / close * .995, r)
+    return maxBuy
     
-# def sellAmountIsolatedMargin(client,ticker):
-#     step = float(client.get_symbol_info(symbol=ticker)["filters"][2]["stepSize"]) # pos in list can change when updated
-#     r = num_of_zeros(step)
-#     balanceSell = float(client.get_isolated_margin_account(symbols=ticker,
-#     recvWindow=10000)['assets'][0]['baseAsset']['free'])
-#     maxSell = round(balanceSell * .995, r)
-#     return maxSell
+def sellAmountFutures(client,ticker):
+    step = float(client.get_symbol_info(symbol=ticker)["filters"][2]["stepSize"]) # pos in list can change when updated
+    r = num_of_zeros(step)
+    balanceSell = float(client.futures_account_balance(recvWindow=10000)[0]['availableBalance'])
+    maxSell = round(balanceSell * .995, r)
+    return maxSell
 
-# def send_message(message, user_name):
-#     return requests.post(
-#         "https://api.mailgun.net/v3/sandboxa9a4e79977d24da59b94080d8e9ace3d.mailgun.org/messages",
-#         auth=("api", "d3743a5f880cb69e598d39673ec3c8bc-a09d6718-4c9c0cf4"),
-#         data={"from": "Breadhooks <mailgun@sandboxa9a4e79977d24da59b94080d8e9ace3d.mailgun.org>",
-#               "to": ["giancarlo.errigo@gmail.com"],
-#               "subject": "We've got a problem",
-#               "text": f"The order for {user_name} could not be filled. {message}"})
+def send_message(message, user_name):
+    return requests.post(
+        "https://api.mailgun.net/v3/sandboxa9a4e79977d24da59b94080d8e9ace3d.mailgun.org/messages",
+        auth=("api", "d3743a5f880cb69e598d39673ec3c8bc-a09d6718-4c9c0cf4"),
+        data={"from": "Breadhooks <mailgun@sandboxa9a4e79977d24da59b94080d8e9ace3d.mailgun.org>",
+              "to": ["giancarlo.errigo@gmail.com"],
+              "subject": "We've got a problem",
+              "text": f"The order for {user_name} could not be filled. {message}"})
 
 
 @app.route('/DLkmvcox') #Laura
@@ -111,9 +118,9 @@ def bot():
 
 # @app.route('/webhook_spot', methods=['POST'])
 # def webhook_spot():
-    
+
 #     data = json.loads(request.data)
- 
+
 #     ticker= data.get('ticker')
 #     side = data.get('side')
 
@@ -129,22 +136,36 @@ def bot():
 #         except:
 #             continue
 
-# @app.route('/webhook_isolated_margin', methods=['POST'])
-# def webhook_isolated_margin():
-    
-#     data = json.loads(request.data)
-    
-#     ticker = data.get('ticker')
-#     side = data.get('side')
+@delay(5)
+@app.route('/futures_entry', methods=['POST'])
+def futures_entry():
 
-#     for user_data in clients:
-#         user_name = user_data["user_name"]
-#         client = user_data["client"]
-#         try:
-#             if side == "BUY":
-#                 quantity = buyAmountIsolatedMargin(client, ticker)
-#             elif side == "SELL":
-#                 quantity = sellAmountIsolatedMargin(client, ticker)
-#             order_response=orderIsolatedMargin(client, side, quantity, ticker, user_name)
-#         except:
-#             continue
+    data = json.loads(request.data)
+
+    ticker = data.get('ticker')
+    side = data.get('side')
+    client = giancarlo
+
+    # if side == "BUY":
+    #     quantity = buyAmountFutures(client, ticker)
+    # elif side == "SELL":
+    #     quantity = sellAmountFutures(client, ticker)
+    order_response = orderFutures(client, side, 10, ticker)
+    
+    return order_response
+
+@app.route('/futures_exit', methods=['POST'])
+def futures_exit():
+
+    data = json.loads(request.data)
+
+    ticker = data.get('ticker')
+    client = giancarlo
+
+    order_response = cancelOrderFutures(client, ticker)
+    
+    return order_response
+
+# what you neeed to do is add a decorator that delays time
+# then you should have two endpoints. 1 for the Entering trades and another for exiting
+# Emtering always has a delay, exiting is immediate
--- config.py
+++ config.py
@@ -1,14 +1,8 @@
 user_credentials = [
     {"user":"giancarlo",
     "API":{
-        "key":"VuWoxAVcNL3tzXEMU60wZ7gU6Pg4Yfx5uo2NFebNV7twpPgJNM0MKGzZPUUZwHlk",
-        "secret":"xBT85IKH7LL3nnicjVDJpVYXU61c3oJ8LYWIFOKVEtRL676l1Bzk3LMt9mbDmqf4"
-        }
-    },
-    {"user":"david",
-    "API":{
-        "key":"IrxHVTbWLvXE6BYWIWUvFtIVCGfilMcd4FYjqfwTdNJZL3PkCNE9SFMnSTLNANNi",
-        "secret":"4L6YSqWCB7iE9i842bE7H7es7Vn0X6Mz5qc4iPR3BX8ysmLITiA2950vejRhnwlH"
+        "key":"RbaEWSWnaWj6VC5vhw5MHkOkJxyPm4ECsjCacYed9yKi2VEHTcOKtMH6BmEasNUd",
+        "secret":"fAVgXNpM9jLmGsivKSkD832gFj2xlpKJLM1EuuKm6xBzURzodAHp0CqmV7cTzXqG"
         }
     }
 ]
